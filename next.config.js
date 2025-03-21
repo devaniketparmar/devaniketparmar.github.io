f5a04822e6cf032c1027a5e5b5d491b9b29840f8
devaniketparmar <mail.aniketparmar.dev@gmail.com>
2025-03-21 20:51:51 +0530

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/devaniketparmar.github.io",
+  assetPrefix: "/devaniketparmar.github.io/",
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
